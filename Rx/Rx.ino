7c7c8e939f996183f8ebf0944d122f2195bf8ccd
bbarber <bbarber@nebraskaglobal.com>
2016-08-19 19:19:04 -0500

    Add rx/tx sample code

---
+++ Rx/Rx.ino
@@ -0,0 +1,51 @@
+#include "pitches.h"
+
+#include <VirtualWire.h>
+
+
+const int beeper_pin = 8;
+const int led_pin = 13;
+//const int transmit_pin = 12;
+const int receive_pin = 11;
+//const int transmit_en_pin = 3;
+
+void setup()
+{
+  delay(1000);
+  Serial.begin(9600);  // Debugging only
+  Serial.println("setup");
+
+  // Initialise the IO and ISR
+  //vw_set_tx_pin(transmit_pin);
+  vw_set_rx_pin(receive_pin);
+  //vw_set_ptt_pin(transmit_en_pin);
+  //vw_set_ptt_inverted(true); // Required for DR3100
+  vw_setup(2000);  // Bits per sec
+
+  vw_rx_start();       // Start the receiver PLL running
+}
+
+void loop()
+{
+  uint8_t buf[VW_MAX_MESSAGE_LEN];
+  uint8_t buflen = VW_MAX_MESSAGE_LEN;
+
+  if (vw_get_message(buf, &buflen)) // Non-blocking
+  {
+    int i;
+    tone(beeper_pin, NOTE_C4, 5);
+    digitalWrite(led_pin, HIGH);
+    delay(100);
+    digitalWrite(led_pin, LOW);
+
+    for (i = 0; i < buflen; i++)
+    {
+      Serial.print(buf[i], HEX);
+      Serial.print(' ');
+    }
+
+    delay(10);
+    Serial.println();
+  }
+}
+
